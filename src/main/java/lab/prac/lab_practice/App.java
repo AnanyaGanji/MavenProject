ae7f29ddbace235bc172ee7dad303d5757b855c4
AnanyaGanji <139428449+AnanyaGanji@users.noreply.github.com>
2025-11-20 04:46:51 +0530

    Add additional print statement to App.java

--- src/main/java/lab/prac/lab_practice/App.java
+++ src/main/java/lab/prac/lab_practice/App.java
@@ -6,5 +6,6 @@ package lab.prac.lab_practice;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Change made!!");
     }
 }
